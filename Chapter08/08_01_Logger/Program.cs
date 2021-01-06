71e0a294af194ae92ee73df528e7412658b5952d
JerryChuang <jerrychuang@nine-yi.com>
2021-01-06 09:41:54 +0800

    create 08_01_Logger project.

---
+++ Chapter08/08_01_Logger/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _08_01_Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debugger.Log(0, null, "This is a test debug message.\n");
+            Console.Read();
+        }
+    }
+}
