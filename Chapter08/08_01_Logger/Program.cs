2e418c560d2308f60450b0845fce2a699d45a5d6
JerryChuang <jerrychuang@nine-yi.com>
2021-01-14 08:42:23 +0800

    implement chapter 8.1.2 lab-1

--- Chapter08/08_01_Logger/Program.cs
+++ Chapter08/08_01_Logger/Program.cs
@@ -10,6 +10,14 @@ namespace _08_01_Logger
             // TODO: What is Debugger and Debug different?
             Debugger.Log(0, null, "This is a test debugger message.\n");
             Debug.WriteLine("This is a test debug message.");
+
+            // chapter 8.1.2
+            // 可用 SourceLevels 列舉類型來針對記錄檔事件等級進行過濾。
+            var source = new TraceSource("Foobar", SourceLevels.All);
+            var eventTypes = Enum.GetValues<TraceEventType>();
+            var eventId = 1;
+            Array.ForEach(eventTypes, et => source.TraceEvent(et, eventId++, $"This is a {et} message."));
+
             Console.Read();
         }
     }
