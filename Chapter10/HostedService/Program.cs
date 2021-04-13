879b8630735eef64a4146f245e1a00c362e0b770
JerryChuang <jerrychuang@glaibo.com>
2021-04-13 23:38:05 +0800

    Chapter10.1.1 承載長時間運行服務。

---
+++ Chapter10/HostedService/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HostedService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new HostBuilder()
+            .ConfigureServices(svcs =>
+                // svcs.AddSingleton<IHostedService, PerformanceMetricsCollector>())
+                svcs.AddHostedService<PerformanceMetricsCollector>())
+            .Build()
+            .Run();
+        }
+    }
+}
